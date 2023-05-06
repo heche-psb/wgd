e6e9662dde7f2ccf634c4cee41927173f29b2a2a
heche-psb <heche@HengchiChen.localdomain>
2023-05-06 23:38:01 +0200

    update PYPI

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='wgd',
-    version='2.0.0.t',
+    version='2.0.1',
     packages=['wgd'],
     url='http://github.com/heche-psb/wgd',
     license='GPL',
